6308c8d8472b93f97cac049f2e594933975ca880
marcos.s.lucas <marcos.s.lucas@8f7c8532-8cd5-11dd-aa62-cdabd1f4ca1e>
2008-10-10 00:32:43 +0000

    nomsg

---
+++ SucaSports/trunk/sql/sqlCreateUsers.sql
@@ -0,0 +1,9 @@
+----------------------------------------
+--
+-- Desc: script de creacion de usuarios
+-- Author: Marcos Lucas
+-- Fecha: 2008109
+--
+----------------------------------------
+
+CREATE USER sucasports@'%' IDENTIFIED BY '9E63XUNu5';
