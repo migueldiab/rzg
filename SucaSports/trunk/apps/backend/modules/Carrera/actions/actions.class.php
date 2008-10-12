a86d4f3dd29c93d98d32f88a1bc70fd67aa3acea
remixtreme <remixtreme@8f7c8532-8cd5-11dd-aa62-cdabd1f4ca1e>
2008-10-12 00:38:30 +0000

    Construi los modulos de Carrera,Categoria y Alquiler para poder tener las dependencias de carrera (no etapa todavia) copie los templates del cache de carrera y empeze a trabajar en el edit carrera para estudiar un poco como se manejan las interacciones entre clases

---
+++ SucaSports/trunk/apps/backend/modules/Carrera/actions/actions.class.php
@@ -0,0 +1,299 @@
+<?php
+
+/**
+ * Carrera actions.
+ *
+ * @package    sucasports
+ * @subpackage Carrera
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class CarreraActions extends autoCarreraActions
+{
+public function executeIndex()
+  {
+    return $this->forward('carrera', 'list');
+  }
+
+  public function executeList()
+  {
+    $this->processSort();
+
+    $this->processFilters();
+
+
+    // pager
+    $this->pager = new sfPropelPager('Carrera', 20);
+    $c = new Criteria();
+    $this->addSortCriteria($c);
+    $this->addFiltersCriteria($c);
+    $this->pager->setCriteria($c);
+    $this->pager->setPage($this->getRequestParameter('page', $this->getUser()->getAttribute('page', 1, 'sf_admin/carrera')));
+    $this->pager->init();
+    // save page
+    if ($this->getRequestParameter('page')) {
+        $this->getUser()->setAttribute('page', $this->getRequestParameter('page'), 'sf_admin/carrera');
+    }
+  }
+
+  public function executeCreate()
+  {
+    return $this->forward('carrera', 'edit');
+  }
+
+  public function executeSave()
+  {
+    return $this->forward('carrera', 'edit');
+  }
+
+
+  public function executeDeleteSelected()
+  {
+    $this->selectedItems = $this->getRequestParameter('sf_admin_batch_selection', array());
+
+    try
+    {
+      foreach (CarreraPeer::retrieveByPks($this->selectedItems) as $object)
+      {
+        $object->delete();
+      }
+    }
+    catch (PropelException $e)
+    {
+      $this->getRequest()->setError('delete', 'Could not delete the selected Carreras. Make sure they do not have any associated items.');
+      return $this->forward('carrera', 'list');
+    }
+
+    return $this->redirect('carrera/list');
+  }
+
+  public function executeEdit()
+  {
+    $this->carrera = $this->getCarreraOrCreate();
+
+    if ($this->getRequest()->isMethod('post'))
+    {
+      $this->updateCarreraFromRequest();
+
+      try
+      {
+        $this->saveCarrera($this->carrera);
+      }
+      catch (PropelException $e)
+      {
+        $this->getRequest()->setError('edit', 'Could not save the edited Carreras.');
+        return $this->forward('carrera', 'list');
+      }
+
+      $this->getUser()->setFlash('notice', 'Your modifications have been saved');
+
+      if ($this->getRequestParameter('save_and_add'))
+      {
+        return $this->redirect('carrera/create');
+      }
+      else if ($this->getRequestParameter('save_and_list'))
+      {
+        return $this->redirect('carrera/list');
+      }
+      else
+      {
+        return $this->redirect('carrera/edit?id='.$this->carrera->getId());
+      }
+    }
+    else
+    {
+      $this->labels = $this->getLabels();
+    }
+  }
+
+  public function executeDelete()
+  {
+    $this->carrera = CarreraPeer::retrieveByPk($this->getRequestParameter('id'));
+    $this->forward404Unless($this->carrera);
+
+    try
+    {
+      $this->deleteCarrera($this->carrera);
+    }
+    catch (PropelException $e)
+    {
+      $this->getRequest()->setError('delete', 'Could not delete the selected Carrera. Make sure it does not have any associated items.');
+      return $this->forward('carrera', 'list');
+    }
+
+    return $this->redirect('carrera/list');
+  }
+
+  public function handleErrorEdit()
+  {
+    $this->preExecute();
+    $this->carrera = $this->getCarreraOrCreate();
+    $this->updateCarreraFromRequest();
+
+    $this->labels = $this->getLabels();
+
+    return sfView::SUCCESS;
+  }
+
+  protected function saveCarrera($carrera)
+  {
+    $carrera->save();
+
+  }
+
+  protected function deleteCarrera($carrera)
+  {
+    $carrera->delete();
+  }
+
+  protected function updateCarreraFromRequest()
+  {
+    $carrera = $this->getRequestParameter('carrera');
+
+    if (isset($carrera['nombre']))
+    {
+      $this->carrera->setNombre($carrera['nombre']);
+    }
+    if (isset($carrera['url']))
+    {
+      $this->carrera->setUrl($carrera['url']);
+    }
+    if (isset($carrera['descripcion']))
+    {
+      $this->carrera->setDescripcion($carrera['descripcion']);
+    }
+    if (isset($carrera['created_at']))
+    {
+      if ($carrera['created_at'])
+      {
+        try
+        {
+          $dateFormat = new sfDateFormat($this->getUser()->getCulture());
+                              if (!is_array($carrera['created_at']))
+          {
+            $value = $dateFormat->format($carrera['created_at'], 'I', $dateFormat->getInputPattern('g'));
+          }
+          else
+          {
+            $value_array = $carrera['created_at'];
+            $value = $value_array['year'].'-'.$value_array['month'].'-'.$value_array['day'].(isset($value_array['hour']) ? ' '.$value_array['hour'].':'.$value_array['minute'].(isset($value_array['second']) ? ':'.$value_array['second'] : '') : '');
+          }
+          $this->carrera->setCreatedAt($value);
+        }
+        catch (sfException $e)
+        {
+          // not a date
+        }
+      }
+      else
+      {
+        $this->carrera->setCreatedAt(null);
+      }
+    }
+    if (isset($carrera['created_by']))
+    {
+      $this->carrera->setCreatedBy($carrera['created_by']);
+    }
+    if (isset($carrera['updated_at']))
+    {
+      if ($carrera['updated_at'])
+      {
+        try
+        {
+          $dateFormat = new sfDateFormat($this->getUser()->getCulture());
+                              if (!is_array($carrera['updated_at']))
+          {
+            $value = $dateFormat->format($carrera['updated_at'], 'I', $dateFormat->getInputPattern('g'));
+          }
+          else
+          {
+            $value_array = $carrera['updated_at'];
+            $value = $value_array['year'].'-'.$value_array['month'].'-'.$value_array['day'].(isset($value_array['hour']) ? ' '.$value_array['hour'].':'.$value_array['minute'].(isset($value_array['second']) ? ':'.$value_array['second'] : '') : '');
+          }
+          $this->carrera->setUpdatedAt($value);
+        }
+        catch (sfException $e)
+        {
+          // not a date
+        }
+      }
+      else
+      {
+        $this->carrera->setUpdatedAt(null);
+      }
+    }
+    if (isset($carrera['updated_by']))
+    {
+      $this->carrera->setUpdatedBy($carrera['updated_by']);
+    }
+  }
+
+  protected function getCarreraOrCreate($id = 'id')
+  {
+    if ($this->getRequestParameter($id) === ''
+     || $this->getRequestParameter($id) === null)
+    {
+      $carrera = new Carrera();
+    }
+    else
+    {
+      $carrera = CarreraPeer::retrieveByPk($this->getRequestParameter($id));
+
+      $this->forward404Unless($carrera);
+    }
+
+    return $carrera;
+  }
+
+  protected function processFilters()
+  {
+  }
+
+  protected function processSort()
+  {
+    if ($this->getRequestParameter('sort'))
+    {
+      $this->getUser()->setAttribute('sort', $this->getRequestParameter('sort'), 'sf_admin/carrera/sort');
+      $this->getUser()->setAttribute('type', $this->getRequestParameter('type', 'asc'), 'sf_admin/carrera/sort');
+    }
+
+    if (!$this->getUser()->getAttribute('sort', null, 'sf_admin/carrera/sort'))
+    {
+    }
+  }
+
+  protected function addFiltersCriteria($c)
+  {
+  }
+
+  protected function addSortCriteria($c)
+  {
+    if ($sort_column = $this->getUser()->getAttribute('sort', null, 'sf_admin/carrera/sort'))
+    {
+      $sort_column = sfInflector::camelize(strtolower($sort_column));
+      $sort_column = CarreraPeer::translateFieldName($sort_column, BasePeer::TYPE_PHPNAME, BasePeer::TYPE_COLNAME);
+      if ($this->getUser()->getAttribute('type', null, 'sf_admin/carrera/sort') == 'asc')
+      {
+        $c->addAscendingOrderByColumn($sort_column);
+      }
+      else
+      {
+        $c->addDescendingOrderByColumn($sort_column);
+      }
+    }
+  }
+
+  protected function getLabels()
+  {
+    return array(
+      'carrera{id}' => 'Id:',
+      'carrera{nombre}' => 'Nombre:',
+      'carrera{url}' => 'Url:',
+      'carrera{descripcion}' => 'Descripcion:',
+      'carrera{created_at}' => 'Created at:',
+      'carrera{created_by}' => 'Created by:',
+      'carrera{updated_at}' => 'Updated at:',
+      'carrera{updated_by}' => 'Updated by:',
+    );
+  }
+}
---
+++ SucaSports/trunk/apps/backend/modules/Carrera/templates/_edit_form.php
@@ -0,0 +1,142 @@
+<form>
+<?php echo form_tag('carrera/save', array(
+  'id'        => 'sf_admin_edit_form',
+  'name'      => 'sf_admin_edit_form',
+  'multipart' => true,
+)) ?>
+
+<?php echo object_input_hidden_tag($carrera, 'getId') ?>
+
+<fieldset id="sf_fieldset_none" class="">
+
+<div class="form-row">
+  <?php echo label_for('carrera[nombre]', __($labels['carrera{nombre}']), '') ?>
+  <div class="content<?php if ($sf_request->hasError('carrera{nombre}')): ?> form-error<?php endif; ?>">
+  <?php if ($sf_request->hasError('carrera{nombre}')): ?>
+    <?php echo form_error('carrera{nombre}', array('class' => 'form-error-msg')) ?>
+  <?php endif; ?>
+
+  <?php $value = object_input_tag($carrera, 'getNombre', array (
+  'size' => 45,
+  'control_name' => 'carrera[nombre]',
+)); echo $value ? $value : '&nbsp;' ?>
+    </div>
+</div>
+<div class="form-row">
+ <!-- <?php echo label_for('carrera[categoria]', __($labels['carrera{categoria}']), '') ?>-->
+Categoria 
+  <div class="content<?php if ($sf_request->hasError('carrera{categoria}')): ?> form-error<?php endif; ?>">
+  <?php if ($sf_request->hasError('carrera{categoria}')): ?>
+    <?php echo form_error('carrera{categoria}', array('class' => 'form-error-msg')) ?>
+  <?php endif; ?>
+
+  <?php $value = object_select_tag($carrera, 'getId', array (
+  'related_class' => 'Categoria',
+  'control_name' => 'categoria[id]',
+  'include_blank' => true,
+)); echo $value ? $value : '&nbsp;' ?>
+    </div>
+</div>
+<div class="form-row">
+  <?php echo label_for('carrera[url]', __($labels['carrera{url}']), '') ?>
+  <div class="content<?php if ($sf_request->hasError('carrera{url}')): ?> form-error<?php endif; ?>">
+  <?php if ($sf_request->hasError('carrera{url}')): ?>
+    <?php echo form_error('carrera{url}', array('class' => 'form-error-msg')) ?>
+  <?php endif; ?>
+
+  <?php $value = object_input_tag($carrera, 'getUrl', array (
+  'size' => 45,
+  'control_name' => 'carrera[url]',
+)); echo $value ? $value : '&nbsp;' ?>
+    </div>
+</div>
+
+<div class="form-row">
+  <?php echo label_for('carrera[descripcion]', __($labels['carrera{descripcion}']), '') ?>
+  <div class="content<?php if ($sf_request->hasError('carrera{descripcion}')): ?> form-error<?php endif; ?>">
+  <?php if ($sf_request->hasError('carrera{descripcion}')): ?>
+    <?php echo form_error('carrera{descripcion}', array('class' => 'form-error-msg')) ?>
+  <?php endif; ?>
+
+  <?php $value = object_textarea_tag($carrera, 'getDescripcion', array (
+  'size' => '30x3',
+  'control_name' => 'carrera[descripcion]',
+)); echo $value ? $value : '&nbsp;' ?>
+    </div>
+</div>
+
+<div class="form-row">
+  <?php echo label_for('carrera[created_at]', __($labels['carrera{created_at}']), '') ?>
+  <div class="content<?php if ($sf_request->hasError('carrera{created_at}')): ?> form-error<?php endif; ?>">
+  <?php if ($sf_request->hasError('carrera{created_at}')): ?>
+    <?php echo form_error('carrera{created_at}', array('class' => 'form-error-msg')) ?>
+  <?php endif; ?>
+
+  <?php $value = object_input_date_tag($carrera, 'getCreatedAt', array (
+  'rich' => true,
+  'withtime' => true,
+  'calendar_button_img' => '/sf/sf_admin/images/date.png',
+  'control_name' => 'carrera[created_at]',
+)); echo $value ? $value : '&nbsp;' ?>
+    </div>
+</div>
+
+<div class="form-row">
+  <?php echo label_for('carrera[created_by]', __($labels['carrera{created_by}']), '') ?>
+  <div class="content<?php if ($sf_request->hasError('carrera{created_by}')): ?> form-error<?php endif; ?>">
+  <?php if ($sf_request->hasError('carrera{created_by}')): ?>
+    <?php echo form_error('carrera{created_by}', array('class' => 'form-error-msg')) ?>
+  <?php endif; ?>
+
+  <?php $value = object_input_tag($carrera, 'getCreatedBy', array (
+  'size' => 7,
+  'control_name' => 'carrera[created_by]',
+)); echo $value ? $value : '&nbsp;' ?>
+    </div>
+</div>
+
+<div class="form-row">
+  <?php echo label_for('carrera[updated_at]', __($labels['carrera{updated_at}']), '') ?>
+  <div class="content<?php if ($sf_request->hasError('carrera{updated_at}')): ?> form-error<?php endif; ?>">
+  <?php if ($sf_request->hasError('carrera{updated_at}')): ?>
+    <?php echo form_error('carrera{updated_at}', array('class' => 'form-error-msg')) ?>
+  <?php endif; ?>
+
+  <?php $value = object_input_date_tag($carrera, 'getUpdatedAt', array (
+  'rich' => true,
+  'withtime' => true,
+  'calendar_button_img' => '/sf/sf_admin/images/date.png',
+  'control_name' => 'carrera[updated_at]',
+)); echo $value ? $value : '&nbsp;' ?>
+    </div>
+</div>
+
+<div class="form-row">
+  <?php echo label_for('carrera[updated_by]', __($labels['carrera{updated_by}']), '') ?>
+  <div class="content<?php if ($sf_request->hasError('carrera{updated_by}')): ?> form-error<?php endif; ?>">
+  <?php if ($sf_request->hasError('carrera{updated_by}')): ?>
+    <?php echo form_error('carrera{updated_by}', array('class' => 'form-error-msg')) ?>
+  <?php endif; ?>
+
+  <?php $value = object_input_tag($carrera, 'getUpdatedBy', array (
+  'size' => 45,
+  'control_name' => 'carrera[updated_by]',
+)); echo $value ? $value : '&nbsp;' ?>
+    </div>
+</div>
+
+</fieldset>
+
+<?php include_partial('edit_actions', array('carrera' => $carrera)) ?>
+
+</form>
+
+<ul class="sf_admin_actions">
+      <li class="float-left"><?php if ($carrera->getId()): ?>
+<?php echo button_to(__('delete'), 'carrera/delete?id='.$carrera->getId(), array (
+  'post' => true,
+  'confirm' => __('Are you sure?'),
+  'class' => 'sf_admin_action_delete',
+)) ?><?php endif; ?>
+</li>
+  </ul>
