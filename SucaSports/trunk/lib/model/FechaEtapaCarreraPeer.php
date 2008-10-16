c3c593ef6c253e1fcef31223c2fc5c46297a43f3
marcos.s.lucas <marcos.s.lucas@8f7c8532-8cd5-11dd-aa62-cdabd1f4ca1e>
2008-10-16 22:49:39 +0000

    nomsg

--- SucaSports/trunk/lib/model/FechaEtapaCarreraPeer.php
+++ SucaSports/trunk/lib/model/FechaEtapaCarreraPeer.php
@@ -9,4 +9,35 @@
  */ 
 class FechaEtapaCarreraPeer extends BaseFechaEtapaCarreraPeer
 {
+     public static function retrieveByCarrera($id_carrera, $con = null)
+	{
+		if ($con === null) {
+			$con = Propel::getConnection(self::DATABASE_NAME);
+		}
+
+		$criteria = new Criteria(FechaEtapaCarreraPeer::DATABASE_NAME);
+
+		$criteria->add(FechaEtapaCarreraPeer::ID_CARRERA, $id_carrera);
+
+
+		$v = FechaEtapaCarreraPeer::doSelect($criteria, $con);
+
+		return !empty($v) > 0 ? $v[0] : null;
+	}
+
+    public static function retrieveByCarreraEtapa($id_carrera, $id_etapa, $con = null)
+	{
+		if ($con === null) {
+			$con = Propel::getConnection(self::DATABASE_NAME);
+		}
+
+		$criteria = new Criteria(FechaEtapaCarreraPeer::DATABASE_NAME);
+        $criteria->add(FechaEtapaCarreraPeer::ID_CARRERA, $id_carrera);
+		$criteria->add(FechaEtapaCarreraPeer::ID_ETAPA_CARRERA, $id_etapa);
+
+
+		$v = FechaEtapaCarreraPeer::doSelect($criteria, $con);
+
+		return !empty($v) > 0 ? $v[0] : null;
+	}
 }
