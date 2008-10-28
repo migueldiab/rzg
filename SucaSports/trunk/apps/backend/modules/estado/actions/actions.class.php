38666d63ff6bc65918089e3464018d1b66487d73
remixtreme <remixtreme@8f7c8532-8cd5-11dd-aa62-cdabd1f4ca1e>
2008-10-28 16:33:01 +0000

    Create Carrera en generator.yml Clases inventario y relacionadas

---
+++ SucaSports/trunk/apps/backend/modules/estado/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * estado actions.
+ *
+ * @package    sucasports
+ * @subpackage estado
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class estadoActions extends autoestadoActions
+{
+}
