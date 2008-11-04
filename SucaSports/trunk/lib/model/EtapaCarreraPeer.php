a4f3a030a2e0761d3b513191bcc215018543826f
marcos.s.lucas <marcos.s.lucas@8f7c8532-8cd5-11dd-aa62-cdabd1f4ca1e>
2008-11-04 23:07:00 +0000

    Reconstruccion del modelos luego de cambios fecha_etapa_carrera de estructuras de la base de datos.

--- SucaSports/trunk/lib/model/EtapaCarreraPeer.php
+++ SucaSports/trunk/lib/model/EtapaCarreraPeer.php
@@ -9,4 +9,15 @@
  */ 
 class EtapaCarreraPeer extends BaseEtapaCarreraPeer
 {
+
+    	public static function retrieveByIdCarrera( $id_carrera, $con = null) {
+		if ($con === null) {
+			$con = Propel::getConnection(self::DATABASE_NAME);
+		}
+		$criteria = new Criteria();
+		$criteria->add(EtapaCarreraPeer::ID_CARRERA, $id_carrera);
+		$v = EtapaCarreraPeer::doSelect($criteria, $con);
+
+		return !empty($v) ? $v[0] : null;
+	}
 }
