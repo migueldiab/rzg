832e2af4d44ec459956b150eef92dc154e5feb4f
remixtreme <remixtreme@8f7c8532-8cd5-11dd-aa62-cdabd1f4ca1e>
2008-11-09 21:50:21 +0000

    Listado de etapas de una carrera dentro del edit carrera. Dos formas incluidas mediante link y en la misma pagina. Esta ultima por ahora solo muestra el array con los datos, no he podido traducirlo en algo legible aun. Agregar etapa todavia no funciona del todo con detalles menores

--- SucaSports/trunk/lib/model/Carrera.php
+++ SucaSports/trunk/lib/model/Carrera.php
@@ -9,6 +9,23 @@
  */ 
 class Carrera extends BaseCarrera
 {
+    public function makelist($arrayname,$tab="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp",$indent=0) {
+        $curtab ="";
+         $returnvalues = "";
+         while(list($key, $value) = each($arrayname)) {
+          for($i=0; $i<$indent; $i++) {
+           $curtab .= $tab;
+           }
+          if (is_array($value)) {
+           $returnvalues .= "$curtab$key : Array: <br />$curtab{<br />\n";
+           $returnvalues .= makelist($value,$tab,$indent+1)."$curtab}<br />\n";
+           }
+          else $returnvalues .= "$curtab$key => $value<br />\n";
+          $curtab = NULL;
+          }
+         return $returnvalues;
+    }
+    
     public function __toString() {
       return $this->getNombre();
     }
@@ -46,5 +63,46 @@ class Carrera extends BaseCarrera
 		}
 		$this->lastCategoriaCarreraCriteria = $criteria;
 		return $this->collCategoriaCarreras;
+	}
+    public function getEtapaCarrera($criteria = null, $con = null)
+	{
+				if ($criteria === null) {
+			$criteria = new Criteria();
+		}
+		elseif ($criteria instanceof Criteria)
+		{
+			$criteria = clone $criteria;
+		}
+
+		if ($this->collEtapaCarreras === null) {
+			if ($this->isNew()) {
+			   $this->collEtapaCarreras = array();
+			} else {
+
+				$criteria->add(EtapaCarreraPeer::ID_CARRERA, $this->getId());
+
+				EtapaCarreraPeer::addSelectColumns($criteria);
+				$this->collEtapaCarreras = EtapaCarreraPeer::doSelect($criteria, $con);
+			}
+		} else {
+						if (!$this->isNew()) {
+												
+
+				$criteria->add(EtapaCarreraPeer::ID_CARRERA, $this->getId());
+
+				EtapaCarreraPeer::addSelectColumns($criteria);
+				if (!isset($this->lastEtapaCarreraCriteria) || !$this->lastEtapaCarreraCriteria->equals($criteria)) {
+					$this->collEtapaCarreras = EtapaCarreraPeer::doSelect($criteria, $con);
+				}
+			}
+		}
+		$this->lastEtapaCarreraCriteria = $criteria;
+        //this->makelist($this->collEtapaCarreras);
+        //foreach ($this->collEtapaCarreras) as $etapa{
+        //    print_r($etapa);
+        //}
+        print_r($this->collEtapaCarreras);
+		return $this->collEtapaCarreras;
+        
 	}
 }
