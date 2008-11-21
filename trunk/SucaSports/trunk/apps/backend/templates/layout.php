e340b15475d51ececc99f9b2562aaa2e6052270f
migueldiab <migueldiab@8f7c8532-8cd5-11dd-aa62-cdabd1f4ca1e>
2008-11-21 01:10:09 +0000

    Dojo refined

--- trunk/SucaSports/trunk/apps/backend/templates/layout.php
+++ trunk/SucaSports/trunk/apps/backend/templates/layout.php
@@ -18,8 +18,14 @@
   </div>
    end contextMenu -->
 
-  <div id="main" dojoType="dijit.layout.BorderContainer" liveSplitters="false" design="sidebar">
-    <h1 id="header" dojoType="dijit.layout.ContentPane" region="top">Suca Sports
+  <div dojoType="dijit.layout.BorderContainer" 
+   id="main" 
+   liveSplitters="false"
+   design="sidebar">
+    <h1 dojoType="dijit.layout.ContentPane" 
+     id="header" 
+     region="top">
+        Suca Sports
         <?php if ($sf_user->isAuthenticated()): ?>
           <div class="float-right"><?php echo button_to(' log out ', 'usuario/logout') ?></div>
           <div class="float-right"><?php echo button_to($sf_user->getAttribute('usuario', '', 'sesion'), 'usuario/edit') ?></div>
@@ -29,21 +35,30 @@
     </h1>
 
     <div dojoType="dijit.layout.AccordionContainer"
-      minSize="20" style="width: 200px;" id="leftAccordion" region="leading" splitter="true">
+      minSize="20"
+      style="width: 200px;"
+      id="leftAccordion"
+      region="leading"
+      splitter="true">
         <?php include_component_slot('sidebar') ?>
     </div><!-- end AccordionContainer -->
 
     <!-- top tabs (marked as "center" to take up the main part of the BorderContainer) -->
-    <div dojoType="dijit.layout.TabContainer" region="center" id="topTabs">
+    <div dojoType="dijit.layout.TabContainer"
+     region="center"
+     d="topTabs">
 
-      <div id="basicFormTab" dojoType="dijit.layout.ContentPane" title="Ventana Principal" style="padding:10px;display:none;">
+      <div dojoType="dijit.layout.ContentPane"
+       id="basicFormTab" 
+       title="Ventana Principal"
+       style="padding:10px;display:none;">
         <?php echo $sf_content ?>
-        <span id="themeData"></span>
+        
       </div>
-    </div><!-- end of region="center" TabContainer -->
-
-
-  </div><!-- end of BorderContainer -->
+    </div>
+    <!-- end of region="center" TabContainer -->
+  </div>
+  <!-- end of BorderContainer -->
 
   <!-- dialog in body -->
   <div id="dialog1" dojoType="dijit.Dialog" 
