0dbac2d14e1593f8ac8f14b3a75e3170b449e1eb
marcos.s.lucas <marcos.s.lucas@8f7c8532-8cd5-11dd-aa62-cdabd1f4ca1e>
2009-01-05 21:03:00 +0000

    In work- Recuperar pwd module

--- SucaSports/trunk/lib/model/UsuarioPeer.php
+++ SucaSports/trunk/lib/model/UsuarioPeer.php
@@ -9,4 +9,25 @@
  */ 
 class UsuarioPeer extends BaseUsuarioPeer
 {
+
+  public static function retrieveByEmail($email, $con = null)
+	{
+		if ($con === null) {
+			$con = Propel::getConnection(self::DATABASE_NAME);
+		}
+
+		$criteria = new Criteria(UsuarioPeer::DATABASE_NAME);
+
+		$criteria->add(UsuarioPeer::EMAIL, $email);
+
+
+      $v = UsuarioPeer::doSelectOne($criteria);
+
+		return !empty($v) > 0 ? $v : null;
+	}
+
+
+
+
+
 }
