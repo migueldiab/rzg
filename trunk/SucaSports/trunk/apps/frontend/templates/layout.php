1996be1eadd325f1e36177bd0ee4d83af2f2e68c
remixtreme <remixtreme@8f7c8532-8cd5-11dd-aa62-cdabd1f4ca1e>
2009-01-08 01:39:31 +0000

    nuevo layout (no se donde referenciar las imagenes para que aparezcan en todo el proyecto por igual) falta mucho todavia y ver si vamos a ir con una estructura asi o vamos a prescindir de la columna izquierda y tener solo el contendio principal antes de empezar a tocar CSS y hacer imagenes

--- trunk/SucaSports/trunk/apps/frontend/templates/layout.php
+++ trunk/SucaSports/trunk/apps/frontend/templates/layout.php
@@ -1,49 +1,54 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-  "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html><head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<?php include_metas() ?>
 
 	<?php include_title() ?>
-
-	<link rel="shortcut icon" href="/favicon.ico" />
+<!--<link href="padrao.css" rel="stylesheet" type="text/css">-->
+<link rel="shortcut icon" href="/favicon.ico" />
 </head>
 <body>
-<center>
-<table width="95%">
-	<tr>
-    <td>
-      <?php echo link_to(image_tag('logo_suca.jpg'),'@homepage') ?>
-    </td>
-    <td valign="top" align="right">
-     <?php include_partial('home/gads'); ?>
-     <br/>
-     <div id="sf_admin_container">
-       <ul class="sf_admin_actions">
-        <?php if ($sf_user->isAuthenticated()): ?>
-          <li class="float-right"><?php echo button_to(' log out ', 'usuario/logout') ?></li>
-          <li class="float-right"><?php echo button_to(' mi perfil ', 'corredor/perfil') ?></li>
-        <?php else: ?>
-          <li class="float-right"><?php echo button_to('log in/registrate', 'usuario/login') ?></li>
-        <?php endif ?>
-        <li class="float-right"><?php echo button_to(' links ', 'home/links') ?></li>
-        <li class="float-right"><?php echo button_to(' team building ', 'home/team') ?></li>
-        <li class="float-right"><?php echo button_to(' entrevistas ', 'home/entrevistas') ?></li>
-        <li class="float-right"><?php echo button_to(' calendario ', 'home/calendario') ?></li>
-        <li class="float-right"><?php echo button_to(' quienes somos? ', 'home/quienes') ?></li>
-        <li class="float-right"><?php echo button_to(' carreras ', 'home/index') ?></li>
-       </ul>
-     </div>
-    </td>
-	</tr>
-	<tr>
-		<td colspan="2">
-      <?php echo $sf_content ?>
-		</td>
-	</tr>
-</table>
-</center>
-</body>
-</html>
+<div id="principal" align="center">
+		<div id="conteudo">
+			<table id="Table_01" width="800" border="0" cellpadding="0" cellspacing="0">
+				<tbody><tr>
+					<td width="25" rowspan="3">
+						<img src="index_03.jpg" width="25" height="800"></td>
+				  <td colspan="2" bgcolor="#1F0176" height="55"><?php echo link_to(image_tag('logo_suca.jpg'),'@homepage') ?></td>
+					<td width="25" rowspan="3">
+						<img src="index_03.jpg" width="25" height="800"></td>
+				</tr>
+				<tr>
+					<td valign="top" width="150" align="center" bgcolor="#666666" height="546">
+					  <br>
+					  <br><br><br><br><br><br><br><br><br><br><br>
+                      <div id="calendario"></div>
+                      <!--script>
+            				d = new Date();
+            				calendario(d.getMonth(), d.getFullYear());
+            		  </script>-->
+					</td>
+			  <td rowspan="2" valign="top" width="600" bgcolor="#666666" height="558">
+		  <div class="c1" id="sf_admin_container">
+                        <ul class="sf_admin_actions">
+                            <?php if ($sf_user->isAuthenticated()): ?>
+                              <li class="float-right"><?php echo button_to(' log out ', 'usuario/logout') ?></li>
+                              <li class="float-right"><?php echo button_to(' mi perfil ', 'corredor/perfil') ?></li>
+                            <?php else: ?>
+                              <li class="float-right"><?php echo button_to('log in/registrate', 'usuario/login') ?></li>
+                            <?php endif ?>
+                            <li class="float-right"><?php echo button_to(' links ', 'home/links') ?></li>
+                            <li class="float-right"><?php echo button_to(' team building ', 'home/team') ?></li>
+                            <li class="float-right"><?php echo button_to(' entrevistas ', 'home/entrevistas') ?></li>
+                            <li class="float-right"><?php echo button_to(' calendario ', 'home/calendario') ?></li>
+                            <li class="float-right"><?php echo button_to(' quienes somos? ', 'home/quienes') ?></li>
+                            <li class="float-right"><?php echo button_to(' carreras ', 'home/index') ?></li>
+                           </ul>
+                        </div>
+                        <br>
+                        <?php echo $sf_content ?>
+				</tr>
+			</tbody></table>
+		</div>
+	</div>
+</body></html>
