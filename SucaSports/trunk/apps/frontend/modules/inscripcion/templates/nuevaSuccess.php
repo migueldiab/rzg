82813f34a707ce508f06998fba9f98ae4784983a
migueldiab <migueldiab@8f7c8532-8cd5-11dd-aa62-cdabd1f4ca1e>
2009-01-20 01:32:35 +0000

    Fix Issue [51]

--- SucaSports/trunk/apps/frontend/modules/inscripcion/templates/nuevaSuccess.php
+++ SucaSports/trunk/apps/frontend/modules/inscripcion/templates/nuevaSuccess.php
@@ -21,20 +21,24 @@
 	<fieldset id="sf_fieldset_none" class="">
 	
 	<div class="form-row">
-  <?php echo label_for('inscripcion[id_corredor]', __('Documento')) ?>	
-  <?php echo object_input_tag($inscripcion, 'getIdCorredor', array('disabled'=>'true')) ?>
+  <?php echo label_for('inscripcion[id_corredor]', __('Corredor')) ?>	
+  <?php echo input_hidden_tag($inscripcion->getIdCorredor()) ?>
+  <?php echo $inscripcion->getCorredor() ?>
   </div>
   <div class="form-row">
   <?php echo label_for('inscripcion[id_carrera]', __('Carrera')) ?> 
-  <?php echo object_input_tag($inscripcion, 'getIdCarrera', array('disabled'=>'true')) ?>
+  <?php echo input_hidden_tag($inscripcion->getIdCarrera()) ?>
+  <?php echo $inscripcion->getCarrera() ?>
   </div>
   <div class="form-row">
-  <?php echo label_for('inscripcion[id_etapa]', __('Etapa')) ?> 
-  <?php echo object_input_tag($inscripcion, 'getIdEtapa', array('disabled'=>'true')) ?>
+  <?php echo label_for('inscripcion[id_etapa]', __('Etapa')) ?>
+  <?php echo input_hidden_tag($inscripcion->getIdEtapa()) ?>
+  <?php echo $inscripcion->getEtapa() ?> 
   </div>
   <div class="form-row">
-  <?php echo label_for('inscripcion[fecha_inicio]', __('Fecha de Inicio')) ?> 
-  <?php echo object_input_tag($inscripcion, 'getFechaInicio', array('disabled'=>'true')) ?>
+  <?php echo label_for('inscripcion[fecha_inicio]', __('Fecha de Inicio')) ?>
+  <?php echo input_hidden_tag($inscripcion->getFechaInicio()) ?>
+  <?php echo $inscripcion->getFechaInicio() ?>
   </div>
   <div class="form-row">
   <?php echo label_for('inscripcion[id_categoria]', __('Categoria')) ?> 
@@ -42,6 +46,7 @@
     'control_name' => 'inscripcion[id_categoria]',
     'related_class' => 'Categoria'
   ), '') ?>
+  </div>
   <div class="form-row">
   <?php echo label_for('cuenta_corriente[id_forma_pago]', __('Forma de Pago')) ?> 
   <?php echo object_select_tag($cuenta_corriente, 'getIdFormaPago', array (
--- SucaSports/trunk/lib/model/Inscripcion.php
+++ SucaSports/trunk/lib/model/Inscripcion.php
@@ -9,4 +9,19 @@
  */ 
 class Inscripcion extends BaseInscripcion
 {
+  public function getCarrera($con = null)
+  {
+    if ($this->aCarrera === null && ($this->id_carrera !== null)) {
+      $this->aCarrera = CarreraPeer::retrieveByPK($this->id_carrera, $con);
+    }
+    return $this->aCarrera;
+  }
+ public function getEtapa($con = null)
+  {
+    if ($this->aEtapa === null && ($this->id_etapa !== null)) {
+      $this->aEtapa = EtapaCarreraPeer::retrieveByPK($this->id_etapa, $this->id_carrera, $con);
+    }
+    return $this->aEtapa;
+  }
+  
 }
