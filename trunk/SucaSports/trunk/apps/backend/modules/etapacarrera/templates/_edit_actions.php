8ffa2024a18cd9e0becb4ad9954651f07cacde6c
remixtreme <remixtreme@8f7c8532-8cd5-11dd-aa62-cdabd1f4ca1e>
2009-01-28 00:07:21 +0000

    Issues 38 y 40 Fixed

--- trunk/SucaSports/trunk/apps/backend/modules/etapacarrera/templates/_edit_actions.php
+++ trunk/SucaSports/trunk/apps/backend/modules/etapacarrera/templates/_edit_actions.php
@@ -2,11 +2,11 @@
     <li><?php echo button_to(__('list'), 'etapacarrera/list?id_etapa='.$etapa_carrera->getIdEtapa().'&id_carrera='.$etapa_carrera->getIdCarrera(), array (
   'class' => 'sf_admin_action_list',
 )) ?></li>
-    <li><?php echo submit_tag(__('save'), array (
+    <li><?php echo submit_tag(__('Guardar/Agregar Fecha de Etapa'), array (
   'name' => 'save',
   'class' => 'sf_admin_action_save',
 )) ?></li>
-      <li><?php echo button_to(__('Agregar Fecha de Etapa'), 'etapacarrera/fecha?id_etapa='.$etapa_carrera->getIdEtapa().'&id_carrera='.$etapa_carrera->getIdCarrera(), array (
+<!--      <li><?php echo button_to(__('Agregar Fecha de Etapa'), 'etapacarrera/fecha?id_etapa='.$etapa_carrera->getIdEtapa().'&id_carrera='.$etapa_carrera->getIdCarrera(), array (
     'class' => 'sf_admin_action_save_and_add',
-)) ?></li>
+)) ?></li>-->
 </ul>
