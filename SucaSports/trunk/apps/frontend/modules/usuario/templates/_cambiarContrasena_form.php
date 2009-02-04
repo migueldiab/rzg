517fb86f98e21656ef2d72c648204cf07cbc0abc
marcos.s.lucas <marcos.s.lucas@8f7c8532-8cd5-11dd-aa62-cdabd1f4ca1e>
2009-02-04 00:45:49 +0000

    -Cambiar contrasena [In work]

--- SucaSports/trunk/apps/frontend/modules/usuario/templates/_cambiarContrasena_form.php
+++ SucaSports/trunk/apps/frontend/modules/usuario/templates/_cambiarContrasena_form.php
@@ -1,7 +1,7 @@
 <?php use_helper('Object', 'Validation', 'ObjectAdmin', 'I18N', 'Date') ?>
 <?php use_stylesheet('/sf/sf_admin/css/main') ?>
 
-<?php echo form_tag('usuario/login', array(
+<?php echo form_tag('usuario/guardarNuevaContrasenia', array(
   'id'        => 'sf_admin_edit_form',
   'name'      => 'sf_admin_edit_form',
   'multipart' => true,
@@ -12,24 +12,24 @@
 <fieldset id="sf_fieldset_none" class="">
 
 <div class="form-row">
-  <?php echo label_for('usuario[password]', __($labels['usuario{password}']), '') ?>
+  <?php echo label_for('nuevaContrasenia', __($labels['usuario{password}']), '') ?>
   <div class="content<?php if ($sf_request->hasError('usuario{password}')): ?> form-error<?php endif; ?>">
   <?php if ($sf_request->hasError('usuario{password}')): ?>
     <?php echo form_error('usuario{password}', array('class' => 'form-error-msg')) ?>
   <?php endif; ?>
 
-  <?php echo input_password_tag('usuario[password]');?>
+  <?php echo input_password_tag('nuevaContrasenia');?>
     </div>
 </div>
 
 <div class="form-row">
-  <?php echo label_for('usuario[verify_password]', __($labels['usuario{verify_password}']), '') ?>
+  <?php echo label_for('nuevaContraseniaVerificar', __($labels['usuario{verify_password}']), '') ?>
   <div class="content<?php if ($sf_request->hasError('usuario{verify_password}')): ?> form-error<?php endif; ?>">
   <?php if ($sf_request->hasError('usuario{verify_password}')): ?>
     <?php echo form_error('usuario{verify_password}', array('class' => 'form-error-msg')) ?>
   <?php endif; ?>
 
-  <?php echo input_password_tag('usuario[verify_password]');?>
+  <?php echo input_password_tag('nuevaContraseniaVerificar');?>
     </div>
 </div>
 
